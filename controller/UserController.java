06358af427804de19a359d5d03a40c0e9c8d9bc0
arifshaik2016 <arifshaik2016@gmail.com>
2026-07-05 22:11:28 +0530

    Added ticket controller changes

--- controller/UserController.java
+++ controller/UserController.java
@@ -2,6 +2,7 @@ package com.BookMyShow.controller;
 
 import java.util.Optional;
 
+import org.apache.catalina.startup.HomesUserDatabase;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,11 @@ import com.BookMyShow.Service.UserService;
 public class UserController {
 	
 	private UserService userService;
+	
+	@GetMapping("/")
+		public String Home() {
+			return "Home";
+		}
 	
 	@PostMapping("/addUser")
 	public User AddUser(@RequestBody User user) throws UserAlreadyExistsException {	
